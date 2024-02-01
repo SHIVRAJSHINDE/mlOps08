1825e7c5c9d76386e6f41b0dda14809669217a58
SHIVRAJ SHINDE <shindeshivraaj@gmail.com>
2024-02-01 18:44:31 +0530

    updated

--- README.md
+++ README.md
@@ -10,3 +10,15 @@ Update the components
 Update the pipeline
 Update the main.py
 Update the dvc.yaml
+
+
+MLFLOW_TRACKING_URI=https://dagshub.com/SHIVRAJSHINDE/mlOps08.mlflow \
+MLFLOW_TRACKING_USERNAME=SHIVRAJSHINDE \
+MLFLOW_TRACKING_PASSWORD=5499747c80911cda6c320b403a03d246f2295149 \
+python script.py
+
+
+export MLFLOW_TRACKING_URI=https://dagshub.com/SHIVRAJSHINDE/mlOps08.mlflow
+export MLFLOW_TRACKING_USERNAME=SHIVRAJSHINDE 
+export MLFLOW_TRACKING_PASSWORD=5499747c80911cda6c320b403a03d246f2295149
+
--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -9,7 +9,16 @@ class DataIngestionConfig:
     local_data_file: Path
     unzip_dir: Path
 
-
+@dataclass(frozen=True)
+class TrainingConfig:
+    root_dir: Path
+    trained_model_path: Path
+    updated_base_model_path: Path
+    training_data: Path
+    params_epochs: int
+    params_batch_size: int
+    params_is_augmentation: bool
+    params_image_size: list
 
 @dataclass(frozen=True)
 class PrepareBaseModelConfig:
@@ -26,19 +35,6 @@ class PrepareBaseModelConfig:
 
 
 @dataclass(frozen=True)
-class TrainingConfig:
-    root_dir: Path
-    trained_model_path: Path
-    updated_base_model_path: Path
-    training_data: Path
-    params_epochs: int
-    params_batch_size: int
-    params_is_augmentation: bool
-    params_image_size: list
-
-
-
-@dataclass(frozen=True)
 class EvaluationConfig:
     path_of_model: Path
     training_data: Path
@@ -46,3 +42,5 @@ class EvaluationConfig:
     mlflow_uri: str
     params_image_size: list
     params_batch_size: int
+
+
